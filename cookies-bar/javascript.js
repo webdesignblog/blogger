9d5b08d4e0b72b4150293105655d6dab0d54fa44
webdesignblog <91596376+webdesignblog@users.noreply.github.com>
2021-09-29 12:26:23 +0300

    Delete cookies-bar directory

--- cookies-bar/javascript.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-setTimeout(function(){
-
-document.getElementsByClassName('cookie-choices-text')[0].innerHTML = "This website is using cookies.";
-
-}, 10);
-
-});
