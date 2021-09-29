c68539e0bda213d06c130bd5f915e478c688b3d3
webdesignblog <91596376+webdesignblog@users.noreply.github.com>
2021-09-29 11:35:10 +0300

    Create javascript.js

---
+++ cookies-bar/javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+setTimeout(function(){
+
+document.getElementsByClassName('cookie-choices-text')[0].innerHTML = "This website is using cookies.";
+
+}, 10);
+
+});
